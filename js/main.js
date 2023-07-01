39d6758cfc291d73a4c292354cd629b5470e941a
Katiuscia Rosa <110988756+Katiusciamr@users.noreply.github.com>
2023-07-01 10:31:35 -0300

    Criado o botão para escolha de cor do robô.

--- js/main.js
+++ js/main.js
@@ -61,3 +61,6 @@ function atualizaEstatistica(peca) {
         elemento.textContent = parseInt(elemento.textContent) + pecas[peca] [elemento.dataset.estatistica]
     })
 }
+function trocaImagem(cor){
+    document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";
+ }
